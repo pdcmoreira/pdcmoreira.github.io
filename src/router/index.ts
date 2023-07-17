e4b8f62bf22d8306a1f6f3270652bb1175bc045f
Pedro Moreira <pdcmoreira@gmail.com>
2023-07-17 15:49:18 +0100

    feat: vue 3 base setup

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import GameView from '@/views/GameView.vue'
+import BoringView from '@/views/BoringView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'game',
+      component: GameView
+    },
+    {
+      path: '/boring-mode',
+      name: 'boring-mode',
+      component: BoringView
+    }
+  ]
+})
+
+export default router
---
+++ src/views/GameView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import JourneyGame from '@/components/JourneyGame'
+</script>
+
+<template>
+  <div class="game-view">
+    <JourneyGame />
+  </div>
+</template>
+
+<style lang="less">
+.game-view {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+}
+</style>
