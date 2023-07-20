a212d2d570037fecf75af36d5b9e180be42df3aa
Pedro Moreira <pdcmoreira@gmail.com>
2023-07-20 16:30:19 +0100

    chore: rename abbreviations

--- src/components/JourneyGame/utilities/backgroundPosition.ts
+++ src/components/JourneyGame/utilities/backgroundPosition.ts
@@ -2,25 +2,25 @@ import { getPixelCoordinatesByIndex } from './coordinates'
 
 export const getPixelBackgroundPositionByIndex = (
   index: number,
-  cols: number,
+  columns: number,
   tileWidth: number,
   tileHeight: number
 ) => {
-  const [x, y] = getPixelCoordinatesByIndex(index, cols, tileWidth, tileHeight)
+  const [x, y] = getPixelCoordinatesByIndex(index, columns, tileWidth, tileHeight)
 
   return `${-x}px ${-y}px`
 }
 
 export const getTileBackgroundPositionByIndex = (
   tileCount: number,
-  cols: number,
+  columns: number,
   tileWidth: number,
   tileHeight: number
 ) => {
   const result: { [key: number]: string } = {}
 
   for (let i = 0; i < tileCount; i++) {
-    result[i] = getPixelBackgroundPositionByIndex(i, cols, tileWidth, tileHeight)
+    result[i] = getPixelBackgroundPositionByIndex(i, columns, tileWidth, tileHeight)
   }
 
   return result
