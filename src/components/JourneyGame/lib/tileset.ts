d6bc926a10c805049469417f18256477790ef96f
Pedro Moreira <pdcmoreira@gmail.com>
2023-07-29 01:30:43 +0100

    chore: extract agnostic helpers into separate module

--- src/components/JourneyGame/lib/tileset.ts
+++ src/components/JourneyGame/lib/tileset.ts
@@ -1,3 +1,4 @@
+import { createImage2D } from './canvas'
 import { getPixelsFromIndex } from './positionCalculations'
 
 export const loadTileSet2D = async (url: string) => {
@@ -37,28 +38,3 @@ export const getTileImageDataFromIndex = (
   width: number,
   height: number
 ) => tileSet2D.getImageData(...getPixelsFromIndex(index, columns, width, height), width, height)
-
-export const createImage2D = (width: number, height: number) => {
-  const canvas = document.createElement('canvas')
-
-  canvas.width = width
-
-  canvas.height = height
-
-  const context = canvas.getContext('2d')
-
-  if (!context) {
-    throw new Error("Couldn't get canvas 2D context")
-  }
-
-  return context
-}
-
-export const copyPixels = (
-  targetCanvas: CanvasRenderingContext2D,
-  x: number,
-  y: number,
-  imageData: ImageData
-) => {
-  targetCanvas.putImageData(imageData, x, y)
-}
