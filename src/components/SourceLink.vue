d6d354afe0d1105ddd916740efcb4f419eb9d13e
Pedro Moreira <pdcmoreira@gmail.com>
2023-08-09 04:06:38 +0100

    feat: add source link

---
+++ src/components/SourceLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <a class="source" href="https://github.com/pdcmoreira/pdcmoreira.github.io" target="_blank">
+    <img
+      src="https://img.shields.io/badge/Source-fff.svg?style=flat-square&logo=github&logoColor=181717"
+    />
+  </a>
+</template>
+
+<style lang="less">
+.source {
+  padding: 4px;
+  background: #fff;
+  border-radius: 4px;
+}
+</style>
