ad9c3654802165d2cc741b560f98388d71e5ea17
Pedro Moreira <pdcmoreira@gmail.com>
2023-08-09 01:41:56 +0100

    feat: add some contact info

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import JourneyGame from '@/components/JourneyGame'
+import ContactInfo from '@/components/ContactInfo.vue'
 </script>
 
 <template>
   <div class="game-view">
     <JourneyGame />
+
+    <ContactInfo />
   </div>
 </template>
 
@@ -16,5 +19,13 @@ import JourneyGame from '@/components/JourneyGame'
   left: 0;
   right: 0;
   height: 100%;
+
+  .contact-info {
+    position: fixed;
+    top: 6px;
+    left: 6px;
+    box-shadow: 13px 12px 19px -16px rgba(0, 0, 0, 0.1);
+    z-index: 20;
+  }
 }
 </style>
