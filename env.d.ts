df63708ed37034e829aef7dd0a564931f02bff2c
Pedro Moreira <pdcmoreira@gmail.com>
2023-08-09 01:04:26 +0100

    feat: enable debug based on env

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_DEBUG_ENABLED: boolean
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
