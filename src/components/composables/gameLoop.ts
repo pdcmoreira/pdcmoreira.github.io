7407b4749d1dbf910b0340e97f8b6d70cd691d6c
Pedro Moreira <pdcmoreira@gmail.com>
2025-02-11 23:27:40 +0000

    feat: normalize movement despite framerate differences between devices

--- src/components/composables/gameLoop.ts
+++ src/components/composables/gameLoop.ts
@@ -1,23 +1,39 @@
 import { onMounted, onUnmounted } from 'vue'
 
-export function useGameLoop(update: Function) {
+export function useGameLoop(update: (delta: number) => void) {
   // Fail-safe flag to stop the loop when unmounted
   let running = false
+
+  // Frame-rate independent delta time multiplier
+  let delta = 0
+
+  let lastTime = 0
 
   onMounted(() => {
     running = true
 
-    function gameLoop() {
+    function gameLoop(currentTime: number) {
       if (!running) {
         return
       }
 
-      update()
+      // Calculate delta
 
+      delta = (currentTime - lastTime) / 1000
+
+      lastTime = currentTime
+
+      // Call the update callback with delta
+      update(delta)
+
+      // Loop
       requestAnimationFrame(gameLoop)
     }
 
-    gameLoop()
+    // Initialize lastTime before the first frame
+    lastTime = performance.now()
+
+    gameLoop(lastTime)
   })
 
   onUnmounted(() => {
