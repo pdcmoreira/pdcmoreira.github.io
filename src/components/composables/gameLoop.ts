afb6dcce3edfd3cc61ca57104689de0495dc396e
Pedro Moreira <pdcmoreira@gmail.com>
2025-02-11 22:45:48 +0000

    feat: safely stop the game loop when unmounted

--- src/components/composables/gameLoop.ts
+++ src/components/composables/gameLoop.ts
@@ -1,13 +1,26 @@
-import { onMounted } from 'vue'
+import { onMounted, onUnmounted } from 'vue'
 
 export function useGameLoop(update: Function) {
+  // Fail-safe flag to stop the loop when unmounted
+  let running = false
+
   onMounted(() => {
+    running = true
+
     function gameLoop() {
+      if (!running) {
+        return
+      }
+
       update()
 
       requestAnimationFrame(gameLoop)
     }
 
-    requestAnimationFrame(gameLoop)
+    gameLoop()
+  })
+
+  onUnmounted(() => {
+    running = false
   })
 }
