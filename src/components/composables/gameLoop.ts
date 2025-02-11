06e5f2fcddf0dc1ef334a9b8750b5c897eb9f007
Pedro Moreira <pdcmoreira@gmail.com>
2025-02-11 23:43:07 +0000

    Improvements + some cleanup (#65)
    * chore: update extension recommendation
    
    * chore: cleanup type usage
    
    * feat: safely stop the game loop when unmounted
    
    * feat: normalize movement despite framerate differences between devices

--- src/components/composables/gameLoop.ts
+++ src/components/composables/gameLoop.ts
@@ -1,13 +1,42 @@
-import { onMounted } from 'vue'
+import { onMounted, onUnmounted } from 'vue'
 
-export function useGameLoop(update: Function) {
+export function useGameLoop(update: (delta: number) => void) {
+  // Fail-safe flag to stop the loop when unmounted
+  let running = false
+
+  // Frame-rate independent delta time multiplier
+  let delta = 0
+
+  let lastTime = 0
+
   onMounted(() => {
-    function gameLoop() {
-      update()
+    running = true
 
+    function gameLoop(currentTime: number) {
+      if (!running) {
+        return
+      }
+
+      // Calculate delta
+
+      delta = (currentTime - lastTime) / 1000
+
+      lastTime = currentTime
+
+      // Call the update callback with delta
+      update(delta)
+
+      // Loop
       requestAnimationFrame(gameLoop)
     }
 
-    requestAnimationFrame(gameLoop)
+    // Initialize lastTime before the first frame
+    lastTime = performance.now()
+
+    gameLoop(lastTime)
+  })
+
+  onUnmounted(() => {
+    running = false
   })
 }
